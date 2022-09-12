b306a27b60bcbcf6040091fcd0b00c4c6fe469d2
Tim Dietz <tdietz986@gmail.com>
2022-09-12 11:26:03 -0700

    This is correct

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -2,7 +2,7 @@ import URI from "urijs";
 
 // /records endpoint
 window.path = "http://localhost:3000/records";
-
+var uri = new URI(window.path);
 // Your retrieve function plus any additional functions go here ...
 
 var apiObjCall = {
@@ -12,14 +12,32 @@ var apiObjCall = {
 
 function retrieve(apiObjCall){  
     //create a promise obj with code for retrieve function
-    new Promise(function(resolve, reject){
+    new Promise(function(resolve, reject){       
         
+        //check if path is successful, if not then throw error code
+        fetch(uri).then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+              }
+              reject();
+        })     
+
+        //search for each obj in the page
         var size = Object.keys(apiObjCall).length;
         for(var x = 0; x++; x<size){
-            fetch(window.path)
+            uri.search(apiObjCall);
+        }
+
+        //obj for returned keys
+        var dataReturn = {
+            ids: 0,
+            open: 0,
+            closedPrimaryCount: 0,
+            previousPage: 0,
+            nextPage: 0
         }
         resolve();
-        reject()
+        
     });
 }
 export default retrieve;
