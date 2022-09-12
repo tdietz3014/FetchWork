3c454e3930e775e054c2d1bdc9754c5a96823a9e
Tim Dietz <tdietz986@gmail.com>
2022-09-12 10:31:52 -0700

    updated managed-records.js with promise addition

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -5,4 +5,21 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
+var apiObjCall = {
+    page: 0,
+    colors: []    
+};
+
+function retrieve(apiObjCall){  
+    //create a promise obj with code for retrieve function
+    new Promise(function(resolve, reject){
+        
+        var size = Object.keys(apiObjCall).length;
+        for(var x = 0; x++; x<=size){
+            fetch(window.path)
+        }
+        resolve();
+        reject()
+    });
+}
 export default retrieve;
